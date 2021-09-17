7e63f19d8760b61b4099d64039c9350f6134a555
Casandra Calderon <angie200896@gmail.com>
2021-09-17 06:54:19 -0400

    Pregunta 1 p4

--- src/problems/Equipos.js
+++ src/problems/Equipos.js
@@ -3,11 +3,13 @@ const Equipos = (Ricks, Mortys) =>{
     const promedio1 = sumatoriaRicks / Ricks.length;
     const sumatoriaMortys = Mortys.reduce(function(a, b){ return a + b;}, 0);
     const promedio2 = sumatoriaMortys / Mortys.length;
-    const resultado = promedio1 > 100 || promedio2 > 100? 
+    const resultado = promedio1 >= 100 || promedio2 >= 100? 
     promedio1 > promedio2 ? 
     `Ganador : Ricks (${promedio1})` : 
-    promedio1 === promedio2 ? 
+    promedio1 === promedio2 ?
+    promedio1 >= 100 && promedio2 >= 100? 
     `EMPATE : Ricks (${promedio1}) - Mortys (${promedio2})` : 
+    `NINGUN EQUIPO GANA` : 
     `Ganador : Mortys (${promedio2})` :
     `Puntuaciones menores a 100`
 
