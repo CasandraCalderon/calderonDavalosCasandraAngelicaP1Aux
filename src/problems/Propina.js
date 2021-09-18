f71c56868a2c6c239d4d1648f7e928f964e2764a
Casandra Calderon <angie200896@gmail.com>
2021-09-18 18:41:35 -0400

    Pregunta 2 p2

--- src/problems/Propina.js
+++ src/problems/Propina.js
@@ -1,16 +1,17 @@
-const calcTip = (tip) => {
-    let propina = (tip > 300) ? 
-    (tip * 20 / 100) : 
-    (tip >= 50) ? 
-    (tip * 15 / 100) : 
-    (tip * 20 / 100);
-    let total = tip + propina;
-    return (`La factura fue de ${tip}, la propina fue de ${propina} y el valor total ${total}`);
+const calcTip = (bills) => {
+    let tips = [];
+    let totals = [];
+    for ( let i = 0 ; i < bills.length ; i++) {
+        tips.push((bills[i] > 300) ? 
+        (bills[i] * 20 / 100) : 
+        (bills[i] >= 50) ? 
+        (bills[i] * 15 / 100) : 
+        (bills[i] * 20 / 100));
+        totals.push(tips [i] + bills[i]);
+        console.log(`La factura fue de ${bills[i]}, la propina fue de ${tips[i]} y el valor total ${totals[i]}`);
+    };
 };
-
 let main = () =>{
-    console.log(calcTip(120));
-    console.log(calcTip(200));
-    console.log(calcTip(275));
+    console.log(calcTip([22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]));
 }
 export default main;
