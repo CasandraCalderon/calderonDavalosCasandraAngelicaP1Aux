0edf067ade43a03635fefa6c7330687addca9d2a
Casandra Calderon <angie200896@gmail.com>
2021-09-18 03:12:22 -0400

    Pregunta 2 Parte 1 (Agregacion de funcion arrow 'calcTip')

--- src/problems/Propina.js
+++ src/problems/Propina.js
@@ -1,13 +1,16 @@
-const tip = 275;
-let propina = (tip > 300) ? 
-(tip * 20 / 100) : 
-(tip >= 50) ? 
-(tip * 15 / 100) : 
-(tip * 20 / 100);
-let total = tip + propina;
-
+const calcTip = (tip) => {
+    let propina = (tip > 300) ? 
+    (tip * 20 / 100) : 
+    (tip >= 50) ? 
+    (tip * 15 / 100) : 
+    (tip * 20 / 100);
+    let total = tip + propina;
+    return (`La factura fue de ${tip}, la propina fue de ${propina} y el valor total ${total}`);
+};
 
 let main = () =>{
-    console.log(`La factura fue de ${tip}, la propina fue de ${propina} y el valor total ${total}`);
+    console.log(calcTip(120));
+    console.log(calcTip(200));
+    console.log(calcTip(275));
 }
 export default main;
