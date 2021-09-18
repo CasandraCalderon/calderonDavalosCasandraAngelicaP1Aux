75d7ea5666d0c41b323d70186d8bd647026d4437
Casandra Calderon <angie200896@gmail.com>
2021-09-17 21:45:31 -0400

    Pregunta 1 p7-p8

--- src/problems/CalcAverage.js
+++ src/problems/CalcAverage.js
@@ -1,7 +1,20 @@
 const CalcAverage = (Ricks, Mortys) => {
-    const promedio1 = Ricks.reduce(function(a, b){ return a + b;}, 0) / Ricks.length;
-    const promedio2 = Mortys.reduce(function(a, b){ return a + b;}, 0) / Mortys.length;
-    return [promedio1, promedio2];
+    const avgRicks = Ricks.reduce(function(a, b){ return a + b;}, 0) / Ricks.length;
+    const avgMortys = Mortys.reduce(function(a, b){ return a + b;}, 0) / Mortys.length;
+    
+    const checkWinner = (avgRicks , avgMortys) => {
+        const resultado = avgRicks >= 100 || avgMortys >= 100? 
+        avgRicks > avgMortys ? 
+        `Los Ricks ganan (${avgRicks} contra ${avgMortys})` : 
+        avgRicks === avgMortys ?
+        avgRicks >= 100 && avgMortys >= 100? 
+        `EMPATE : Ricks (${avgRicks}) - Mortys (${avgMortys})` : 
+        `NINGUN EQUIPO GANA` : 
+        `Los Mortys ganan (${avgMortys} contra ${avgRicks})` :
+        `Puntuaciones menores a 100: Ricks (${avgRicks}) Mortys (${avgMortys})`
+        return resultado;
+    };
+    return checkWinner(avgRicks,avgMortys);
 };
 let main = () =>{
     console.log(CalcAverage([96, 108, 89], [88, 91, 110]));
